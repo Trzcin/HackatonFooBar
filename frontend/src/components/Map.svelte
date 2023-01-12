fae0306d9e1b6a5012de7ab90647d3a62912a379
Trzcin <trzccin@gmail.com>
2023-01-12 16:30:29 +0100

    geoencoding working

--- frontend/src/components/Map.svelte
+++ frontend/src/components/Map.svelte
@@ -1,28 +1,54 @@
 <script lang="ts">
+  import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+  import type { Result } from "@mapbox/mapbox-gl-geocoder";
   import mapboxgl from "mapbox-gl";
   import {onMount} from "svelte";
   
+  let lon: number;
+  let lat: number;
+  let data;
+
   onMount(() => { 
     mapboxgl.accessToken = "pk.eyJ1IjoidHJ6Y2luMSIsImEiOiJjbGN0M2pneTkwOWxhM3BxdDZsOWpkdGZ6In0.5Ot2WYhpkxNyNZA8b28xgw";
 
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [54.35, 18.6667],
+      center: [18.6667, 54.35],
       zoom: 13
     });
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+    });
+
+    map.addControl(geocoder);
+
+    geocoder.on('result', (e) => {
+      const result: Result = e.result;
+      lon = result.center[0];
+      lat = result.center[1];
+    })
   });
 
-  
 </script>
 
 <div id="map"></div>
+{#if lat && lon}
+  <h1>{lon} {lat}</h1>
+{/if}
 
 <style>
   #map {
     position: absolute;
     top: 4rem;
-    bottom: 0;
-    widows: 100%;
+    height: calc(100vh - 4rem);
+    width: 100%;
+  }
+
+  h1 {
+    position: absolute;
+    z-index: 0;
   }
 </style>
