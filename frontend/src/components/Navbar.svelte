7a446c8db93646da774009d7a9f0c63ba7e18e8e
Trzcin <trzccin@gmail.com>
2023-01-12 14:48:28 +0100

    first map test

---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,29 @@
+<nav>
+  <div id="logo">
+    <img src="/vite.svg" alt="logo">
+    <p>15 Minute City</p>
+  </div>
+</nav>
+
+<style>
+  nav {
+    width: 100%;
+    height: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1.5rem;
+    box-shadow: 0 5px 5px #33333322;
+  }
+
+  #logo {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  img {
+    width: 2.5rem;
+    height: auto;
+  }
+</style>
