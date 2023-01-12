abf01d04eefba3de8cc89b7358b3142000803ee9
Trzcin <trzccin@gmail.com>
2023-01-12 14:53:09 +0100

    works

--- frontend/src/components/Map.svelte
+++ frontend/src/components/Map.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
   import mapboxgl from "mapbox-gl";
+  import {onMount} from "svelte";
+  
+  onMount(() => { 
+    mapboxgl.accessToken = "pk.eyJ1IjoidHJ6Y2luMSIsImEiOiJjbGN0M2pneTkwOWxhM3BxdDZsOWpkdGZ6In0.5Ot2WYhpkxNyNZA8b28xgw";
 
-  mapboxgl.accessToken = "pk.eyJ1IjoidHJ6Y2luMSIsImEiOiJjbGN0M2pneTkwOWxhM3BxdDZsOWpkdGZ6In0.5Ot2WYhpkxNyNZA8b28xgw";
-
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [54.35, 18.6667],
-    zoom: 13
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [54.35, 18.6667],
+      zoom: 13
+    });
   });
 
   
