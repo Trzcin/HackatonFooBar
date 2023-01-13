27ccec51f24fdd9e0fa9f46d8fc7bb9ef29a1cd4
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2023-01-13 02:44:14 +0100

    Add csv to list Parser

--- backend/DataInitializer/CsvManager.cs
+++ backend/DataInitializer/CsvManager.cs
@@ -1,7 +1,22 @@
-﻿namespace DataInitializer
+﻿using EFDataAccessLibrary.Models;
+
+namespace DataInitializer
 {
     public class CsvManager
     {
-
+        static public List<Place> GetListOfPlaceFromCsvFile(string path, string category)
+        {
+            return File.ReadAllLines(path)
+                .Skip(1)
+                .Select(line => line.Split(','))
+                .Select(row => new Place
+                {
+                    Lat = Convert.ToDouble(row[0]),
+                    Lon = Convert.ToDouble(row[1]),
+                    Category = category,
+                    Title = row[3],
+                    Link = row[4]
+                }).ToList();
+        }
     }
 }
